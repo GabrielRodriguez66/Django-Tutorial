c439e075bd91e6512ccefd62b7ac647fe75aacab
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-20 12:48:17 +0800

    Delete django_tutorial directory

--- django_tutorial/custom_settings/production_settings.py
+++
@@ -1,8 +0,0 @@
-import dj_database_url
-from django_tutorial.settings import *
-
-DEBUG = False
-
-ALLOWED_HOSTS = ['polls-app-tutorial.herokuapp.com', ]
-
-DATABASES['default'].update(dj_database_url.config())
